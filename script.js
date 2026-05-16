43ac6e988b63d345f090b90ebb6631d28e81e6db
RAJESH G R <rgr98624@gmail.com>
2026-05-16 09:21:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+function calculateAttendance() {
+
+    let total = parseInt(document.getElementById("totalnumber").value);
+
+    let attend = parseInt(document.getElementById("attended").value);
+
+    let target = parseFloat(document.getElementById("percent").value);
+
+    let result = document.getElementById("result");
+
+
+    if (
+        isNaN(total) ||
+        isNaN(attend) ||
+        isNaN(target) ||
+        total <= 0 ||
+        attend < 0 ||
+        target <= 0
+    ) {
+
+        result.innerHTML = "⚠️ Please Enter Valid Details";
+        return;
+    }
+
+
+    let currentattendance = ((attend / total) * 100).toFixed(2);
+
+    let message = ` 📊 Current Attendance: ${currentattendance}% <br><br>`;
+
+
+    if (currentattendance >= target) {
+
+        let bunk = Math.floor((attend * 100 / target) - total);
+
+        message += ` 🎉 You can bunk ${bunk} more classes safely.`;
+
+    } else {
+
+        let extraclasses = 0;
+
+        while (
+            ((attend + extraclasses) / (total + extraclasses)) * 100 < target
+        ) {
+
+            extraclasses++;
+        }
+
+        message += ` 📚 Attend next ${extraclasses} classes to reach ${target}%`;
+    }
+
+
+    if (currentattendance < target) {
+
+        message += `<br><br> 🚨Warning: Your attendance is below ${target}%`;
+    }
+
+
+    result.innerHTML = message;
+}
